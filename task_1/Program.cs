29619a3f2638adfc8e35aa2ce1e3ba7c70e18a44
XeniaX <ksusha.hrisanova@yandex.ru>
2022-09-04 18:37:24 +0300

    task1 updated

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -4,10 +4,10 @@ int number1 = int.Parse(Console.ReadLine());
 Console.Write("Введи второе число: ");
 int number2 = int.Parse(Console.ReadLine());
 if (number1 < number2) {
-    Console.WriteLine($"Максимальое число - {number2}, минимальное число - {number1}");
+    Console.WriteLine($"Максимальное число - {number2}, минимальное число - {number1}");
     }
 if (number2 < number1) {
-    Console.WriteLine($"Максимальое число - {number1}, минимальное число - {number2}");
+    Console.WriteLine($"Максимальное число - {number1}, минимальное число - {number2}");
     }
 else {
     Console.WriteLine("Числа равны");
