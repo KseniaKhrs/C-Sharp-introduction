e45f7899a3dbea108151b0e3d2cc859a949a6d0f
XeniaX <ksusha.hrisanova@yandex.ru>
2022-09-08 20:28:45 +0300

    task3 in HW3 tasks added

---
+++ HW_3/task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число 
+//(N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int[] GetCubesSequence(int num) {
+   int[] cubes = new int [num];
+   for (int i = 0; i < num; i++) {
+    cubes[i] = (i+1)*(i+1)*(i+1);
+   }
+   return cubes;
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+for (int i = 0; i < number; i++) Console.Write($"{GetCubesSequence(number)[i]} ");
