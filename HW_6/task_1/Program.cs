d63082bb68ff537a88fa5ac9cce287c25e6f642b
XeniaX <ksusha.hrisanova@yandex.ru>
2022-09-19 18:53:55 +0300

    HW6 added

---
+++ HW_6/task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int GetPositiveQuantity(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите количество вводимых чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+Console.WriteLine($"Введите числа поочередно, каждое число с новой строки: ");
+for (int i = 0; i < m; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int quantity = GetPositiveQuantity(array);
+Console.WriteLine($"Количество положительных чисел равно {quantity}.");
---
+++ HW_6/task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+double[] GetIntersection(double numK1, double numB1, double numK2, double numB2)
+{
+    double[] solution = new double[2];
+    solution[0] = (numB2 - numB1) / (numK1 - numK2);
+    solution[1] = numK1 * solution[0] + numB1;
+    return solution;
+}
+
+Console.WriteLine("Введите параметры для двух прямых (y = k*x + b). Введите k1: ");
+double numberK1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите b1: ");
+double numberB1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите k2: ");
+double numberK2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите b2: ");
+double numberB2 = Convert.ToDouble(Console.ReadLine());
+
+double[] intersection = GetIntersection(numberK1, numberB1, numberK2, numberB2);
+Console.WriteLine($"Прямые пересекаются в точке ({intersection[0]},{intersection[1]}).");
