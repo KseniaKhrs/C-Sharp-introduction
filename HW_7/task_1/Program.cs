18bf079a04ffa0a8c10cc4c369bc5b8f5f7d4ba1
XeniaX <ksusha.hrisanova@yandex.ru>
2022-09-22 22:31:26 +0300

    HW7 added

---
+++ HW_7/task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GetArray (int m, int n, int min, int max)
+{
+    double[,] resultArray = new double[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            resultArray[i, j] = Math.Round((new Random().NextDouble()*(max - min) + min), 2);
+        }
+    }
+    return resultArray;
+}
+int m = 3;
+int n = 4;
+int min = -1000;
+int max = 1000;
+
+double[,] array = GetArray(m, n, min, max);
+for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
---
+++ HW_7/task_3/Program.cs
@@ -0,0 +1,39 @@
+﻿//  Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int [,] GetArray (int m, int n, int min, int max)
+{
+    int[,] resultArray = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            resultArray[i, j] = new Random().Next(min, max);
+        }
+    }
+    return resultArray;
+}
+
+double[] GetMeanColumn(int[,] array)
+{
+    double [] resultArray = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            resultArray[j] += array[i,j];
+        }
+        resultArray[j] = Math.Round((resultArray[j] / array.GetLength(0)),2);
+    }
+    return resultArray;
+}
+
+Console.WriteLine ("Введите число строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите число столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray (m, n, 0, 10);
+
+double[] result = GetMeanColumn(array);
+for (int i = 0; i < result.Length; i++) Console.Write($"{result[i]}; ");
