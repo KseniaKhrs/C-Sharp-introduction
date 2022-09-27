06a9a5075b7d2575b01f07c077bd24aeb8a020d6
XeniaX <ksusha.hrisanova@yandex.ru>
2022-09-27 17:19:39 +0300

    HW8 added

---
+++ HW_8/task_4/Program.cs
@@ -0,0 +1,62 @@
+﻿// .Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+int [,,] GetArray (int x, int y, int z, int min, int max)
+{
+    int[,,] resultArray = new int[x,y,z];
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++) 
+            {
+                int count = 1;
+                int randomNumber = 0;
+                while (count!=0) 
+                {
+                    randomNumber = new Random().Next(min, max);
+                    count = 0;
+                    for (int l = 0; l < x; l++) 
+                    {
+                        for (int m = 0; m < y; m++)
+                        {
+                            for (int n = 0; n < z; n++)
+                            {
+                                if (randomNumber == resultArray[l,m,n]) count++;
+                            }
+                        }
+                    }
+                }         
+                resultArray[i,j,k] = randomNumber;
+            }
+        }
+    }
+    return resultArray;
+}
+
+Console.WriteLine ("Введите измерение x матрицы: ");
+int numX = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите измерение y матрицы: ");
+int numY = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите измерение z матрицы: ");
+int numZ = Convert.ToInt32(Console.ReadLine());
+
+
+int[,,] array = GetArray (numX, numZ, numY, 0, 28);
+
+
+Console.WriteLine("Сгенирирована следующая матрица: ");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            Console.Write($"{array[i,j,k]} ({i}, {j}, {k})   ");
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ HW_8/task_5/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int [,] GetSpiralArray(int row, int column, int minNum)
+{
+    int[,] array = new int[row,column];
+    int i = 0;
+    int j = 0;
+    array [i,j] = minNum;
+    while (i < array.GetLength(0) && i >= 0 && j < array.GetLength(1) && j >= 0)
+    {
+        if ((j+1) < array.GetLength(1) && array[i,j+1] == 0 && ((i-1) < 0 || array[i-1,j] != 0 ))
+        {
+                    array[i,j+1] = array[i,j] + 1;
+            j = j + 1;
+        }
+        else if ((i+1) < array.GetLength(0) && array[i+1,j] == 0) 
+        {
+            array[i+1,j] = array[i,j] + 1;
+            i = i + 1;
+        }
+        else if ((j-1) >= 0 && array[i,j-1] == 0) 
+        {
+            array[i,j-1] = array[i,j] + 1;
+            j = j - 1;
+        }
+        else if ((i-1) >= 0 && array[i-1,j] == 0) 
+        {
+            array[i-1,j] = array[i,j] + 1;
+            i = i - 1;
+        }
+        else break;
+    }
+    return array;
+}
+
+Console.WriteLine ("Введите число строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите число столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите первое число в спиральном массиве: ");
+int minNum = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetSpiralArray (m, n, minNum);
+
+Console.WriteLine("Сгенирирована следующая матрица: ");
+for (int x = 0; x < array.GetLength(0); x++)
+{
+    for (int y = 0; y < array.GetLength(1); y++)
+    {
+        Console.Write(array[x,y] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
